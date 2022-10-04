f8b2eb7485b98831786b8bc2c048d8970e2c1f09
Raquel Cantinha <107546444+raquelcantinha@users.noreply.github.com>
2022-10-04 19:14:13 +0100

    Add files via upload

---
+++ curriculum.js
@@ -0,0 +1,9 @@
+const academicEducationText = document.getElementById("academicEducationText");
+const extraCurricularText = document.getElementById("extraCurricularText");
+const professionalExperienceText = document.getElementById("professionalExperienceText");
+const technicalSkillsText = document.getElementById("technicalSkillsText");
+const languagesText = document.getElementById("languagesText");
+
+function openProjectInfo(i) {
+    i.classList.toggle("guardar__conteudo");
+  }
