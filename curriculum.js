8c40529b41335719feaaef555758c998388930e6
Raquel Cantinha <107546444+raquelcantinha@users.noreply.github.com>
2022-10-13 12:50:21 +0100

    Delete curriculum.js

--- curriculum.js
+++
@@ -1,9 +0,0 @@
-const academicEducationText = document.getElementById("academicEducationText");
-const extraCurricularText = document.getElementById("extraCurricularText");
-const professionalExperienceText = document.getElementById("professionalExperienceText");
-const technicalSkillsText = document.getElementById("technicalSkillsText");
-const languagesText = document.getElementById("languagesText");
-
-function openProjectInfo(i) {
-    i.classList.toggle("guardar__conteudo");
-  }
